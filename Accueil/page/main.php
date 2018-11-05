93cb135fe13a418159a64ab833d67607cc662e6e
Gerard KESSE <tiakagerard@hotmail.com>
2018-11-05 12:24:36 +0100

    Initial Commit

--- Accueil/page/main.php
+++ Accueil/page/main.php
@@ -9,7 +9,8 @@ padding: 50px 10px;
 
 
 </div>
-</div><br></div></div><br><div class="Content0 GHtml1"><div style="
+</div><br></div></div><br><div class="Content0 GHtml1"><div style="padding: 0px 10px;">
+<div style="
 background-color: rgba(255,255,255,0.2);
 max-width: 500px;
 margin: auto;
@@ -19,5 +20,6 @@ padding: 50px 10px;
 <h1>Chorale<br>Église Harriste de France</h1><br><br>Psaumes 150:1-6<br>Louez l'Éternel! Louez Dieu dans son sanctuaire! Louez-le dans l'étendue, où éclate sa puissance! Louez-le pour ses hauts faits! Louez-le selon l'immensité de sa grandeur! Louez-le au son de la trompette! Louez-le avec le luth et la harpe! <br><br><br>
 Bienvenue <br>sur l'interface web dédiée à la 
 <br>Chorale de l’Église Harriste de France.<br><br><br>Restez en contact avec vos Choristes.<br>Découvrez le programme de leur plan d'action.<br>Informez-vous sur leurs projets et leurs activités.<br>Consultez leur répertoire de chants.<br><br><br>Chorale<br>
+</div>
 </div>
 </div><br>
